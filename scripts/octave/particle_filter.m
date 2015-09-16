b1995fe135e68ec77c484b6df86d1b4ddfe51e34
Bruno Faúndez Valenzuela <gameover_sxt_nah@hotmail.com>
2015-09-16 10:48:20 -0300

    Kalman Filter in octave.

--- scripts/octave/particle_filter.m
+++ scripts/octave/particle_filter.m
@@ -13,13 +13,14 @@ particle_stddev = 1.0;
 % Initialize particles
 for i=1:n_particles
   particles(i) = normrnd(0, particle_stddev);
+  stddevs(i) = particle_stddev + normrnd(0, 0.01);
   weights(i) = 1/n_particles;
 endfor
 
 sz = size(samples)(1);
-true_value = ones(1, sz, "uint8") * 2;
+true_value = ones(1, sz, 'uint8') * 2;
 
-epsilon = 0.0000000000000001
+epsilon = 0.0000000000000001;
 for i=1:sz
   % Update weights
   for j=1:n_particles
@@ -41,10 +42,24 @@ for i=1:sz
 
   % Resample particles
   for j=1:n_particles
-    mu_prime = particles(j) + eap;
-    particles(j) = normrnd(eap, particle_stddev);
+    r = unifrnd(0,1);
+    s = 0;
+    k = 0;
+    while(s<r)
+      k = k + 1;
+      s = s + weights(k);
+      %fprintf('R: %f, S: %f K: %d\n', r, s, k)
+    endwhile
+    mu_prime = particles(k);
+    stddev_prime = stddevs(k);
+    particles(j) = normrnd(mu_prime, stddev_prime);
+    stddevs(j) = stddev_prime + normrnd(0, 0.01);
+
+    %mu_prime = eap;
+    %particles(j) = normrnd(mu_prime, particle_stddev);
   endfor
 
+  avgs(i) = sum(eaps(:))/(size(eaps(:))(1));
   waitbar(i/sz)
 endfor
 
@@ -58,5 +73,6 @@ hold on
 plot(samples(:,1), maps(:), '-b', 'linewidth', 2)
 plot(samples(:,1), eaps(:), '-r', 'linewidth', 2)
 plot(samples(:,1), true_value(:), '-g', 'linewidth', 2)
-legend('EAP Estimator', 'MAP Estimator', 'True value')
+plot(samples(:,1), avgs(:), '-c', 'linewidth', 2)
+legend('EAP Estimator', 'MAP Estimator', 'True value', 'Average EAP')
 hold off
